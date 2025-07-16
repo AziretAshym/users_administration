ad0a2917b6817d1fc4b81be909b0e5968cb58b5e
Ashymov Aziret <aziretashym@gmail.com>
2025-07-16 15:19:18 +0600

    Created user model.

---
+++ models/User.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const Schema = mongoose.Schema;
+
+const SALT_WORK_FACTOR = 10;
+const JWT_SECRET = process.env.JWT_SECRET || "secretKey";
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    first_name: String,
+    last_name: String,
+    gender: {
+        type: String,
+        enum: ['male', 'female'],
+    },
+    birthdate: Date,
+    token: {
+        type: String,
+        required: true,
+    },
+});
+
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function () {
+    const payload = {
+        id: this._id,
+        username: this.username,
+    };
+
+    this.token = jwt.sign(payload, JWT_SECRET, {
+        expiresIn: "7d",
+    });
+};
+
+UserSchema.set("toJSON", {
+    transform: (_doc, ret) => {
+        delete ret.password;
+        return ret;
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
