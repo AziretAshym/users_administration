da92812711870b14bc6e241b2003e928ab63ffcd
Ashymov Aziret <aziretashym@gmail.com>
2025-07-17 14:40:32 +0600

    Server configured, added routes, middleware, sessions, and views.

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const config = {
-    db: "mongodb://localhost/user_administration",
+    port: process.env.PORT || 8000,
+    db: process.env.MONGO_URI || "mongodb://localhost:27017/user_administration",
+    jwtSecret: process.env.JWT_SECRET || "secretKey",
+    sessionSecret: process.env.SESSION_SECRET || "sessionSecret",
 };
 
 export default config;
--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 import express from "express";
 import * as mongoose from "mongoose";
-import config from "./config";
-import MongoDb from "./mongoDb";
+import path from "path";
+import session from "express-session";
+import config from "./config.js";
+import MongoDb from "./mongoDb.js";
+import usersRouter from "./routes/usersRoutes.js";
+import authRouter from "./routes/authRoutes.js";
 
 
 const app = express();
 const port = 8000;
+const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: 'yourSecret',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+        },
+    })
+);
 
+app.set("views", path.join(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use("/", authRouter);
+app.use("/users", usersRouter);
+app.use((req, res) => {
+    res.status(404).render("404");
+});
 
 const run = async () => {
     await mongoose.connect(config.db);
     app.listen(port, () => {
-        console.log(`Server started on port http://localhost:${port}`);
+        console.log(`Server started on http://localhost:${port}`);
     });
     process.on("exit", () => {
         MongoDb.disconnect();
